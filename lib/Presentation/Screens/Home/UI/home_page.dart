f15f03837441c9cedfd3b35d99dda2491e5c5f95
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-06-13 15:21:41 +0530

    created constants.dart and created methods for dimensions

---
+++ lib/Presentation/Screens/Home/UI/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:devhabit/Presentation/Components/constants.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = getScreenWidth(context);
+    double screenHeight = getScreenheight(context);
+    return Scaffold();
+  }
+}
