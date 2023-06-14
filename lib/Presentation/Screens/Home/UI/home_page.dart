f61cac31704920ec9375f3338abcfc818e1de80e
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-06-15 00:21:26 +0530

    created appbar inside homepage

--- lib/Presentation/Screens/Home/UI/home_page.dart
+++ lib/Presentation/Screens/Home/UI/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:devhabit/Presentation/Components/export_components.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +14,38 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: whiteColor,
-      body: Center(
-        child: Text('HomePage'),
+      appBar: AppBar(
+        toolbarHeight: getScreenheight(context) * 0.1,
+        backgroundColor: whiteColor,
+        elevation: 0,
+        title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'devhabit.',
+              style: GoogleFonts.leagueSpartan(
+                  color: blackColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: getScreenWidth(context) * 0.08),
+            )),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: getScreenWidth(context) * 0.03),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FeatherIcons.user,
+                color: blackColor,
+              ),
+            ),
+          ),
+        ],
       ),
+      backgroundColor: whiteColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      )
     );
   }
 }
