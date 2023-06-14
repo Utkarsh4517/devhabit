6bdb59a535fcd300a3f3c89e689cff74940439d9
Utkarsh4517 <uttkarsh.dav@gmail.com>
2023-06-14 18:41:43 +0530

    created file navrail.dart

---
+++ lib/Presentation/Components/navrail.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class NavRail extends StatefulWidget {
+  const NavRail({super.key});
+
+  @override
+  State<NavRail> createState() => _NavRailState();
+}
+
+class _NavRailState extends State<NavRail> {
+
+  int _selectedInded = 0;
+
+  // list of widgets to scroll
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
